85b6765b24eaae5a91d18875c94c178b6b17a1b7
Francisco Javier González López <31509775+Ing-Mk-FranJa07@users.noreply.github.com>
2017-10-30 15:24:46 -0500

    Update RNA_Emotions_BodyPosture_Keras_Tensorflow.py

--- Nueral Networks/Classify emotions/RNA_Emotions_BodyPosture_Keras_Tensorflow.py
+++ Nueral Networks/Classify emotions/RNA_Emotions_BodyPosture_Keras_Tensorflow.py
@@ -21,6 +21,41 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import SGD
 
+# Wrappers used to plot the confusion matrix.
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import itertools
+
+def Plot_Confusion_Matrix(Matrix, Title, Cmap, Normalize = 'True'):
+    '''
+    Function that allow plot the confusion matrix, in two formats, 
+    percentage and total data.
+    '''
+    
+    if Normalize:
+        Confusion_Matrix = Matrix.astype('float') / Matrix.sum(axis=1)[:, np.newaxis]
+    else:
+        Confusion_Matrix = Matrix
+        
+    plt.imshow(Confusion_Matrix, interpolation = 'nearest', cmap = Cmap)
+    plt.title(Title)
+    plt.colorbar()
+    tick_marks = np.arange(len(Class_Name))
+    plt.xticks(tick_marks, Class_Name, rotation = 45)
+    plt.yticks(tick_marks, Class_Name)
+    
+    fmt = '.2f' if Normalize else 'd' 
+    thresh = Confusion_Matrix.max() / 2.
+    
+    for i, j in itertools.product(range(Confusion_Matrix.shape[0]), range(Confusion_Matrix.shape[1])):
+        plt.text(j, i, format(Confusion_Matrix[i, j], fmt), horizontalalignment = "center", color = "white" if Confusion_Matrix[i, j] > thresh else "black")
+    
+    plt.tight_layout()
+    plt.ylabel('Desired Output')
+    plt.xlabel('Estimated Output')
+    
+    plt.show()
+
 # Are loaded the Train and Test data.
 Data_Train = pd.read_csv("...\DataSet_Organized\DataSetPostures_Train.csv", header = 0, index_col = 0)
 Data_Test = pd.read_csv("...\DataSet_Organized\DataSetPostures_Test.csv", header = 0, index_col = 0)
@@ -30,6 +65,10 @@ X_Train = np.array(Data_Train.ix[:,2:25])
 Y_Train = np.array(Data_Train.ix[:,25:33])
 X_Test = np.array(Data_Test.ix[:,2:25])
 Y_Test = np.array(Data_Test.ix[:,25:33])
+
+# Are created vectors that contain the index of the each emotion class.
+Index_Y_Test = np.empty([len(Y_Test),1], dtype = np.float64)
+Index_Y_Pred = np.empty([len(Y_Test),1], dtype = np.float64)
 
 # Are defined the parameters of the neural network and the model.
 HL_1_Nodes = 23
@@ -61,6 +100,23 @@ Y_Pred = RNA.predict(X_Test); print('Prediction of the output of the test data',
 # Is evaluated the perform of the neural network.
 Score = RNA.evaluate(X_Test, Y_Test, verbose = 2) 
 print('Accuracy of the neural network', '\n', Score); print('\n'*4)
+
+# Is determined the index of the each class of the output test and output predicted.
+for i in range(len(Y_Test)):
+    Index_Y_Test[i] = np.argmax(Y_Test[i,:]) 
+    Index_Y_Pred[i] = np.argmax(Y_Pred[i,:])
+
+# Is calculated the confusion matrix.
+Confusion_Matrix = confusion_matrix(Index_Y_Test,Index_Y_Pred)
+
+# Are determined the set up to plot the confusion matrix.
+Class_Name = ['Normal', 'Reflexive', 'Surprised', 'Angry', 'Sad', 'Happy']
+Title = 'Confusion Matrix'
+Cmap = plt.cm.Blues
+
+# Are printed the confusion matrix using the both formats.
+Plot_Confusion_Matrix(Confusion_Matrix, Title, Cmap, Normalize = True)
+Plot_Confusion_Matrix(Confusion_Matrix, Title, Cmap, Normalize = False)
 
 # Is saved the model of the neural network.
 Version = '1' # Version of the model created.
